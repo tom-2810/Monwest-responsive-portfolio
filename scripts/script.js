8cfef6965051b551501f0d022dbb73811389f90c
tom-2810 <tom.poortman@hva.nl>
2023-01-21 21:30:52 +0100

    project window
    fullscreen effect centered on project.
    data of project in window and fullscreen.

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,32 @@
-let fullscreen = document.querySelector(".fullscreen");
+let fullscreenFeatured = document.querySelector(".fullscreen.featured");
 let featured = document.querySelector(".featured");
 
-fullscreen.addEventListener("click", () => {
-    fullscreen.classList.toggle("open");
-    fullscreen.classList.toggle("close");
+let featuredContainer = document.querySelector(".project-container#featured");
+
+fullscreenFeatured.addEventListener("click", () => {
+    fullscreenFeatured.classList.toggle("open");
+    fullscreenFeatured.classList.toggle("close");
+    document.body.classList.toggle("noscroll");
 });
 
 featured.addEventListener("click", () => {
-    // featured.style.visibility = "hidden";
-    if(fullscreen.classList.contains("closed")) {
-        fullscreen.classList.toggle("closed");
+    let featuredContainerWidth = featuredContainer.clientWidth;
+    let featuredContainerHeight = featuredContainer.clientHeight;
+
+    let featuredContainerX = featuredContainer.getBoundingClientRect().left;
+    let featuredContainerY = featuredContainer.getBoundingClientRect().top;
+
+    console.log("X: " + featuredContainerX, "Y: " + featuredContainerY);
+    fullscreenFeatured.style.clipPath = `circle(0rem at ${featuredContainerX + (featuredContainerWidth / 2)}px ${featuredContainerY + (featuredContainerHeight / 2)}px)`
+
+    if (fullscreenFeatured.classList.contains("closed")) {
+        fullscreenFeatured.classList.toggle("closed");
     }
 
-    if(fullscreen.classList.contains("close")) {
-    fullscreen.classList.toggle("close");
+    if (fullscreenFeatured.classList.contains("close")) {
+        fullscreenFeatured.classList.toggle("close");
     }
 
-    fullscreen.classList.toggle("open");
+    fullscreenFeatured.classList.toggle("open");
+    document.body.classList.toggle("noscroll");
 });
