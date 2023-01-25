d371e855f8a3524de4568f32b4bd95bc5f702c60
tom-2810 <tom.poortman@hva.nl>
2023-01-25 14:38:12 +0100

    papers and contact sections

--- scripts/open.js
+++ scripts/open.js
@@ -5,6 +5,8 @@ let featuredFullscreen = document.querySelector(".fullscreen.featured");
 
 featuredContainer.addEventListener("click", () => {
     document.body.classList.toggle("noscroll");
+    document.querySelector("main").style.overflowX = "hidden";
+
 
     let featuredContainerX = featuredContainer.getBoundingClientRect().left;
     let featuredContainerY = featuredContainer.getBoundingClientRect().top;
@@ -18,6 +20,8 @@ featuredContainer.addEventListener("click", () => {
         if(featuredFullscreen.classList.contains("open")) {
             featuredFullscreen.style.transform = null;
         } else {
+    document.querySelector("main").style.overflowX = "visible";
+
             featuredFullscreen.style.transform = `translateX(-${featuredContainerX}px) translateY(${-1*featuredContainerY}px)`;
             console.log(featuredContainerY)
         }
@@ -26,11 +30,12 @@ featuredContainer.addEventListener("click", () => {
     if(featuredFullscreen.classList.contains("open")) {
         featuredFullscreen.style.transform = null;
     } else {
+    document.querySelector("main").style.overflowX = "visible";
+
         featuredFullscreen.style.transform = `translateX(-${featuredContainerX}px) translateY(-${featuredContainerY}px)`;
     }
 
         
-    
     featuredFullscreen.style.transition = ".2s ease";
     featuredFullscreen.classList.toggle("open");
     
